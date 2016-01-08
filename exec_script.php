8b65eceef91f92e78a04fd131401be7c256a0948
Jonathan Brem <jon.brem@t-online.de>
2016-01-08 15:05:57 +0100

    Renamed index.php, added comments to exec script

--- exec_script.php
+++ exec_script.php
@@ -3,13 +3,16 @@
 	$out;
 	$return_var;
 
+	// get params
 	$execPath = $_GET["exec_path"];
 	$img = $_GET["img"];
 	$toFile = $_GET["toFile"];
 
+	// execute program
 	exec("$execPath $img $toFile",
 		$out, $return_var);
 
+	// return in json format
 	echo "[";
 
 	if($return_var == 0) {
